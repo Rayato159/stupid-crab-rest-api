dfe4032708d0e0d4f3181d02cf6f7bfabb356ec3
Rayato159 <rayato159@gmail.com>
2023-05-23 21:12:34 +0700

    -refactor :package: add status_code on handlers and add CORS

--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,39 @@
 use std::net::SocketAddr;
 use axum::{
+    http::{Method},
     routing::get,
+    response::{Json, IntoResponse},
     Router,
 };
+use serde_json::{json};
+use tower_http::cors::{CorsLayer, Origin};
 
 pub mod products;
 
 #[tokio::main]
 pub async fn start() {
-    let app = Router::new().route("/:product_id", get(products::handlers::find_one_product));
+    let app = Router::new()
+        .route("/", get(|| async move {
+            Json(json!({
+                "app": "stupid-crab-rest-api",
+                "version": "v0.1.0",
+            })).into_response()
+        })).layer(
+            CorsLayer::new()
+                .allow_origin(Origin::exact(
+                    "http://localhost:5005".parse().unwrap(),
+                ))
+                .allow_methods(
+                    [
+                        Method::GET,
+                        Method::POST,
+                        Method::PUT,
+                        Method::PATCH,
+                        Method::DELETE
+                    ]
+                ),
+        )
+        .route("/:product_id", get(products::handlers::find_one_product));
     
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("server is running on -> {:?}", addr);
--- src/server/products/handlers.rs
+++ src/server/products/handlers.rs
@@ -1,19 +1,27 @@
 use axum::{
-    response::{Json, IntoResponse, Response},
+    response::{Json, IntoResponse},
     extract::{Path},
+    http::{StatusCode},
 };
 use serde_json::json;
 
 use super::entities;
 use super::usecases;
 
-pub async fn find_one_product(Path(product_id): Path<i32>) -> Response {
+pub async fn find_one_product(Path(product_id): Path<i32>) -> impl IntoResponse {
     let product_id_int = product_id.abs();
     match usecases::find_one_product(product_id_int) {
-        entities::Result::Ok(product) => Json(product).into_response(),
-        entities::Result::Err(e) => Json(json!({
-            "status": 200,
-            "message": e,
-        })).into_response(),
+        entities::Result::Ok(product) => {
+            (StatusCode::OK, Json(product).into_response())
+        },
+        entities::Result::Err(e) => {
+            (
+                StatusCode::BAD_REQUEST,
+                Json(json!({
+                    "status": 200,
+                    "message": e,
+                })).into_response()
+            )
+        },
     }
 }
